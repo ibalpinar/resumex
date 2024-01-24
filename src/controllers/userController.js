0c3eb0c0c8d758f0c6ee0bc02af6931869a5d5ce
ibalpinar <ilker@balpinar.com>
2024-01-24 22:07:10 +0300

    Collect objects in data key

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,19 @@
 const User = require('../models/User');
 
+/**
+ * TODO: If there is no user return http 404
+ * when calling the fetch all user and get
+ * one user endpoint
+ * Also return 404 on update and delete when
+ * the user is not found.
+*/
+
 module.exports = {
    create: async (request, reply) => {
       try{
          const user = request.body;
          const newUser = await User.create(user);
-         reply.code(201).send(newUser);
+         reply.code(201).send({ data: newUser });
       }catch(e){
          reply.code(500).send(e);
       }
@@ -14,7 +22,7 @@ module.exports = {
    fetch: async (request, reply) => {
       try{
          const users = await User.find({});
-         reply.code(200).send(users);
+         reply.code(200).send({ data: users });
       }catch(e){
          reply.code(500).send(e);
       }
@@ -24,7 +32,7 @@ module.exports = {
       try{
          const userId = request.params.id;
          const user = await User.findById(userId);
-         reply.code(200).send(user);
+         reply.code(200).send({ data: user });
       }catch(e){
          reply.code(500).send(e);
       }
