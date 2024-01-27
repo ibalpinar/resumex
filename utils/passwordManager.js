4c9fabbb767510e25f9a1a2ec94b2ce34de01b8a
ibalpinar <ilker@balpinar.com>
2024-01-27 21:04:18 +0300

    Remove unnecessary try-catch block and comment

--- utils/passwordManager.js
+++ utils/passwordManager.js
@@ -3,33 +3,14 @@ const bcrypt = require("bcrypt");
 const hashPassword = async function(password, saltRounds) {
    let rhash;
    try {
-      try {
-         const salt = await bcrypt.genSalt(parseInt(saltRounds));
-         const hash = await bcrypt.hash(password, salt);
-         rhash = hash;
-      } catch (err) {
-         console.error(err.message);
-      } finally {
-         return rhash;
-      }
+      const salt = await bcrypt.genSalt(parseInt(saltRounds));
+      const hash = await bcrypt.hash(password, salt);
+      rhash = hash;
    } catch (err) {
       console.error(err.message);
+   } finally {
+      return rhash;
    }
- }
-
- module.exports.hashPassword = hashPassword;
-
-/*
-module.exports.validatePassword = function(hash) {
-   bcrypt
-      .compare(password, hash)
-      .then(res => {
-         console.log('Validated -> ', res);
-         return true;
-      })
-      .catch(err => {
-         console.error(err.message);
-         return false;
-      });
 }
-*/
+
+module.exports.hashPassword = hashPassword;
