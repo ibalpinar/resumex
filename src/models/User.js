334cfa5c5a504e0197aa6b5193979c2775bfce3f
ibalpinar <ilker@balpinar.com>
2024-02-06 23:46:25 +0300

    Add unique feature for email address

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
     name: {type: String, required: true},
     lastName: {type: String, required: true},
     password: {type: String, required: true},
-    email: {type: String, required: true}
+    email: {type: String, unique : true, required: true}
 });
 
 const Users = model('User', UserSchema);
