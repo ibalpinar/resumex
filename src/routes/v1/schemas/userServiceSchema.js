5bd63e6b30599efa01158190b58bdee6cc312862
ibalpinar <ilker@balpinar.com>
2024-02-13 02:59:58 +0300

    Add all simple swagger details of endpoints

--- src/routes/v1/schemas/userServiceSchema.js
+++ src/routes/v1/schemas/userServiceSchema.js
@@ -1,14 +1,16 @@
 const {
 	responseErrors,
 	getSuccessObject,
+   path
 } = require("./common");
 
 const errors = responseErrors;
 
 const userServiceSchema = {
 	createUser: {
-		description: "User creation endpoint. Returns all user information as object.",
-		tags: ["Create User"],
+		description: "User creation endpoint. Returns all user information as object after creation",
+      summary: "Create a user",
+		tags: [path.USER],
 		body: {
 			type: "object",
 			properties: {
@@ -18,15 +20,37 @@ const userServiceSchema = {
 					type: "string",
 					format: "email"
 				},
-				password: { type: "string"}
+				password: { type: "string"},
+            confirmPassword: { type: "string"}
 			},
 			required: ["name", "lastName", "email", "password"]
 		}
 	},
-   fetch: {},
-   get: {},
-   update: {},
-   delete: {},
+   fetch: {
+		description: "This endpoint lists all users. It returns an array of user objects",
+      summary: "Fetch all users",
+		tags: [path.USER]
+   },
+   get: {
+      description: "This endpoint fetches a user given its Id. It returns a single user object",
+      summary: "Get a user",
+		tags: [path.USER]
+   },
+   update: {
+      description: "This endpoint updates user information given its Id. The password does not have to be sent. It is only updated if it is sent. Returns the updated user information as a user object",
+      summary: "Update a user",
+		tags: [path.USER]
+   },
+   delete: {
+      description: "This endpoint deletes a user given its Id. Returns the deleted user's information",
+      summary: "Delete a user",
+		tags: [path.USER]
+   },
+   deleteAllUsers: {
+      description: "This endpoint deletes all users. It only returns a status message",
+      summary: "Delete all users",
+		tags: [path.USER]
+   }
 }
 
 module.exports = {
