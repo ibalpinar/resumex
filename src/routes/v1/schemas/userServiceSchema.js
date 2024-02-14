53b0f918d2255933632680e1686bf1a9ed5feef6
ibalpinar <ilker@balpinar.com>
2024-02-14 03:03:11 +0300

    Add all possible errors for user schema

--- src/routes/v1/schemas/userServiceSchema.js
+++ src/routes/v1/schemas/userServiceSchema.js
@@ -1,9 +1,5 @@
-const {
-	responseErrors,
-	getSuccessObject,
-   path
-} = require("./common");
-
+const { responseErrors, getSuccessObject, path } = require("./common");
+const { responseMessage } = require("../../../utils/responseHelpers");
 const errors = responseErrors;
 
 const userServiceSchema = {
@@ -23,13 +19,38 @@ const userServiceSchema = {
 				password: { type: "string"},
             confirmPassword: { type: "string"}
 			},
-			required: ["name", "lastName", "email", "password"]
-		}
+			required: ["name", "lastName", "email", "password", "confirmPassword"]
+		},
+      response: {
+         201: {
+            description: responseMessage.USER_CREATED_SUCCESSFULLY,
+            type: 'object',
+            properties: {
+                  _id: { type: 'number', format: 'uuid' },
+                  name: { type: "string"},
+                  lastName: { type: "string"},
+                  email: {
+                     type: "string",
+                     format: "email"
+                  }
+            }
+         },
+         400: {
+            description: responseMessage.BAD_REQUEST,
+            type: 'object',
+            properties: responseMessage.DEFAULT_ERROR_RESPONSE_FOR_SCHEMA
+         },
+         500: {
+            description: responseMessage.INTERNAL_SERVER_ERROR,
+            type: 'object',
+            properties: responseMessage.DEFAULT_ERROR_RESPONSE_FOR_SCHEMA
+         }
+      }
 	},
    fetch: {
 		description: "This endpoint lists all users. It returns an array of user objects",
       summary: "Fetch all users",
-		tags: [path.USER]
+		tags: [path.USER],
    },
    get: {
       description: "This endpoint fetches a user given its Id. It returns a single user object",
@@ -39,7 +60,21 @@ const userServiceSchema = {
    update: {
       description: "This endpoint updates user information given its Id. The password does not have to be sent. It is only updated if it is sent. Returns the updated user information as a user object",
       summary: "Update a user",
-		tags: [path.USER]
+		tags: [path.USER],
+		body: {
+			type: "object",
+			properties: {
+				name: { type: "string"},
+            lastName: { type: "string"},
+				email: {
+					type: "string",
+					format: "email"
+				},
+				password: { type: "string"},
+            confirmPassword: { type: "string"}
+			},
+			required: ["name", "lastName", "email", "password"]
+		}
    },
    delete: {
       description: "This endpoint deletes a user given its Id. Returns the deleted user's information",
