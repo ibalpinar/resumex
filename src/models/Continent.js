eb84632c8cdb13be4752b63bb27925f2ab8e1ad5
ibalpinar <ilker@balpinar.com>
2024-02-29 18:45:55 +0300

    Add continent model

---
+++ src/models/Continent.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const ContinentSchema = new Schema({
+   name: {
+      type: String,
+      required: [true, "Name is required field!"],
+      trim: true
+   },
+   code:{
+      type: String,
+      trim: true
+   },
+   createdAt: {
+      type : Date,
+      default: Date.now,
+      trim: true
+   },
+   updatedAt: {
+      type : Date,
+      default: null,
+      trim: true
+   },
+   deletedAt: {
+      type : Date,
+      default: null,
+      trim: true
+   }
+});
+
+const Continent = model('Continent', ContinentSchema);
+
+module.exports = Continent;
