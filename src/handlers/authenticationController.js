4eb490485133386017673d50439f09fa8e26047f
ibalpinar <ilker@balpinar.com>
2024-03-05 03:01:41 +0300

    Add forgot password request endpoint

--- src/handlers/authenticationController.js
+++ src/handlers/authenticationController.js
@@ -1,6 +1,10 @@
 const User = require('../models/User');
+const ForgorPasswordRequest = require('../models/ForgorPasswordRequest');
 const constants = require('../utils/constants');
 const { sendErrorResponse, sendSuccessResponse, checkObjectIdRegExp, responseMessage } = require("../utils/responseHelpers");
+const crypto = require("crypto");
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Types;
 
 module.exports = {
    generateToken: async (request, reply) => {
@@ -40,8 +44,32 @@ module.exports = {
       }
    },
 
-   forgotPassword: async (request, reply) => {},
+   forgotPassword: async (request, reply) => {
+      const email = request.body.email;
+      try{
+         const user = await User.findOne({ email: email }).select(constants.selectUserFields);
+         if(user){
+            let expiredAt = new Date();
+            expiredAt.setHours(expiredAt.getHours() + 24);
+            const forgotPasswordRequestData = {
+               userId: new ObjectId(user.userId),
+               email: user.email,
+               requestToken: crypto.randomBytes(32).toString("hex"),
+               expiredAt: expiredAt
+            }
+            const forgotPasswordRequest = await ForgorPasswordRequest.create(forgotPasswordRequestData);
+            sendSuccessResponse(
+               reply, { statusCode: 200, message: responseMessage.USER_LOGGED_IN_SUCCESSFULLY, data: { forgotPasswordRequest } }
+            );
+         }else{
+            sendErrorResponse(reply, 404, responseMessage.NO_USER_FOUND);
+         }
+      }catch(err){
+         console.error(err.message);
+         sendErrorResponse(reply, 500, responseMessage.INTERNAL_SERVER_ERROR);
+      }
+   },
 
-   setPassword: async (request, reply) => {}
+   resetPassword: async (request, reply) => {}
 
 };
--- src/routes/v1/authenticationRoutes.js
+++ src/routes/v1/authenticationRoutes.js
@@ -70,7 +70,7 @@ const authenticationRoutes = async (app, opts) => {
       method: "POST",
       url: "/setPassword",
       preHandler: [app.verifyEmailPassword],
-      handler: authenticationController.setPassword
+      handler: authenticationController.resetPassword
    });
 
 };
