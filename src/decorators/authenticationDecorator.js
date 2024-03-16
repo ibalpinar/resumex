efa49409d44fda7cc2a38c0e0cc709a8ba6c2d12
ibalpinar <ilker@balpinar.com>
2024-03-16 21:09:17 +0300

    Split decorations two resources

---
+++ src/decorators/authenticationDecorator.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+const constants = require('../utils/constants');
+const error = require('../utils/errors');
+const { sendErrorResponse, responseMessage } = require("../utils/responseHelpers");
+const { comparePassword } = require('../utils/passwordManager');
+
+module.exports = {
+   authenticate: async (request, reply) => {
+      try{
+         await request.jwtVerify();
+      }catch(err){
+         console.error(err.message);
+         if(err.code == error.FST_JWT_AUTHORIZATION_TOKEN_INVALID){
+            sendErrorResponse(reply, 401, responseMessage.AUTHORIZATION_TOKEN_INVALID);
+         }else{
+            sendErrorResponse(reply, 500, responseMessage.INTERNAL_SERVER_ERROR);
+         }
+      }
+   }
+
+};
